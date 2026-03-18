7e5678e50d1a8bf6187aa4c480e03c2d3373a95a
devano <elazzouziadnan4@gmil.com>
2026-03-18 14:42:25 +0000

    commit

---
+++ renforcement/semaine_1/renforcement/Challenge2_relevéNotes.js
@@ -0,0 +1,48 @@
+let eleves = [
+  { 
+    nom: "adnan", note: 10 
+    
+  },
+  { 
+    nom: "sami", note: 2
+  },
+  { 
+    nom: "maroih", note: 5
+  }
+];
+
+let totalNote = 0;
+let max = eleves[0].note;
+let min = eleves[0].note;
+let counter = 0;
+
+for (let i = 0; i < eleves.length; i++) {
+  let n = eleves[i].note;
+  console.log(n)
+
+
+  totalNote += n;
+
+  if (n > max) {
+    max = n;
+  }
+
+  if (n < min) {
+    min = n;
+  }
+
+  if (n > 10) {
+    counter++;
+  }
+}
+
+let moyen = totalNote / eleves.length;
+console.log("=============================");
+console.log("total =", totalNote);
+console.log("=============================");
+console.log("moyenne =", moyen);
+console.log("=============================");
+console.log("max=", max);
+console.log("=============================");
+console.log("min =", min);
+console.log("les succes =", counter);
